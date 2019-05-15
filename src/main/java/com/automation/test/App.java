6073544a0bf6b90c1ac029f6051c1b7a67c42907
jyothi0804 <jburugu@CGI.com>
2019-05-15 16:19:42 +0530

    first commit

---
+++ src/main/java/com/automation/test/App.java
@@ -0,0 +1,13 @@
+package com.automation.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
